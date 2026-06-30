3fbae04731e71d64705bc2789bfd66d19f309765
Stiventre96 <123702741+Stiventre96@users.noreply.github.com>
2026-06-30 09:30:51 -0500

    Update 2_🛒_Ventas_y_Pedidos.py

--- pages/2_🛒_Ventas_y_Pedidos.py
+++ pages/2_🛒_Ventas_y_Pedidos.py
@@ -115,7 +115,7 @@ with tab1:
                 with colE:
                     ext_order_id = st.text_input("ID de Pedido (Ej. Woocommerce)")
                 with colF:
-                    payment_method = st.selectbox("Método de Pago", ["Efectivo", "Wompy", "Contra Entrega", "Bancolombia", "Nequi", "Davivienda", "Canje por Publicidad", "sistecredito", "Embajador" , "consignacion"])
+                    payment_method = st.selectbox("Método de Pago", ["Efectivo", "Wompy", "Epayco", "Contra Entrega", "Bancolombia", "Nequi", "Davivienda", "Canje por Publicidad", "sistecredito", "Embajador" , "consignacion"])
 
                 submit_order = st.form_submit_button("Confirmar Pedido")
 
